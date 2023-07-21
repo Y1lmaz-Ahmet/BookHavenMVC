2ea42eb180f4b3bb2569f8c192077ca05097e236
y1lmaz-Ahmet <ay0201998@gmail.com>
2023-07-21 13:19:23 +0200

    model classes & EF migration

--- BookHavenMVCUI/Data/Migrations/20230721111731_added-tabled.Designer.cs
+++ BookHavenMVCUI/Data/Migrations/20230721111731_added-tabled.Designer.cs
@@ -4,6 +4,7 @@ using BookHavenMVCUI.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BookHavenMVCUI.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230721111731_added-tabled")]
+    partial class addedtabled
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
